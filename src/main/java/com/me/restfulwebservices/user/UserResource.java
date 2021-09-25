91501d0cb90bb50be73354752bd52c97d51d104f
Jhonathan Salazar <50059680+JhonathanAlfonso@users.noreply.github.com>
2021-09-25 11:31:46 -0500

    basic user controller and exception handler

---
+++ src/main/java/com/me/restfulwebservices/user/UserResource.java
@@ -0,0 +1,44 @@
+package com.me.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoService userDaoService;
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return userDaoService.findAll();
+    }
+
+    @GetMapping("/users/{userId}")
+    public User retrieveUser(@PathVariable int userId) {
+        User user = userDaoService.findOne(userId);
+
+        if (user == null) {
+            throw new UserNotFoundException("id-" + userId);
+        }
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User userToSave) {
+        User savedUser = userDaoService.save(userToSave);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+}
